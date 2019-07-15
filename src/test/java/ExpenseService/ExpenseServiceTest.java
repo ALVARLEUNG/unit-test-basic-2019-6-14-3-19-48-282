a8eea92bc0bcebfe7e6e8070dd6655418a6439bb
ALVARLEUNG <alvar.leung@qq.com>
2019-07-15 19:46:35 +0800

    test: add test should_return_other_expense_type_if_project_is_external_and_has_other_name

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -55,8 +55,11 @@ class ExpenseServiceTest {
     @Test
     void should_return_other_expense_type_if_project_is_external_and_has_other_name() throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.EXTERNAL, "Project");
         // when
+        ExpenseType expenseType = expenseService.getExpenseCodeByProjectTypeAndName(project);
         // then
+        Assertions.assertEquals(ExpenseType.OTHER_EXPENSE, expenseType);
     }
 
     @Test
